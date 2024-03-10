ab6718a29b4c6c2f7c265a5545bfd64cf5f6c6a5
Mohit Hurmade <144951649+snoozysapien@users.noreply.github.com>
2024-03-10 08:58:03 +0000

    datatypes

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 144553
+let accountEmail = "hurmademohit@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+// accountId = 2
+
+accountEmail = "mohithurmade18@gmail.com"
+accountPassword = "21212121"
+accountCity = "Bhopal"
+
+// console.log(accountId);
+// console.log(accountEmail);
+// console.log(accountCity);
+
+console.table([accountEmail, accountId, accountPassword, accountCity, accountState])
+//to print multiple variable
---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all js code as newer version of JS
+// alert("hello") can't use this here bcoz we are using node.js not browser
+
+// Datatypes
+
+let name = "Mohit"
+let age = 18
+let isLoggedIn = false
+
+/* primitive data types : 
+   1.number
+   2.bigint(for large no.)
+   3.string(single or double quote)
+   4.boolean(true or false)
+   5.null(stand alone value, empty value)
+   6.undefined(value is not assigned)
+   7.symbol(unique)
+
+   non primitive : 
+   8.object
+*/
+
+console.log(typeof "Mohit");
+console.log(typeof null);
+console.log(typeof undefinned);
+
+
+
+
